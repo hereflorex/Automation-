7ab06f25ee3626bbdc77d39e77a2d47ee5f7b3e2
SIR FLOREX 🪄 <hereflorex@gmail.com>
2026-09-17 16:32:28 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
 import uvicorn
-uvicorn.run("app.main:app", host="0.0.0.0", port=int(os.getenv("PORT","8000")), reload=False)
+
+uvicorn.run(
+    "app.main:app",
+    host="0.0.0.0",
+    port=int(os.getenv("PORT", "8000")),
+    reload=False,
+)
